b7ede52417115790eeac14688cf6b9b4cd30751f
Rupam Das <rejisterjack@gmail.com>
2025-01-23 09:27:21 +0530

    navbar started

--- client/src/components/navbar.component.jsx
+++ client/src/components/navbar.component.jsx
@@ -1,7 +1,15 @@
 import React from "react"
+import logo from "../imgs/logo.png"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="logo" className="w-10" />
+      </Link>
+    </nav>
+  )
 }
 
 export default Navbar
